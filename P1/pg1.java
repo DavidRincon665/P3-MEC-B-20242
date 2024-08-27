0850890d5d8f0d57637686350f626ca375c5a64f
DavidRincon665 <159587372+DavidRincon665@users.noreply.github.com>
2024-08-27 15:20:22 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,25 @@
+/*Crear una clase llamada triangulo con atributos base y altura aplicar encapsulamiento y los metodos apropiados para inicializar o crear objetos con su respectivos atributos */
+package javaapplication1;
+
+
+public class Triangulo {
+
+    public static void main(String[] args) {
+        int base =0;
+        int altura =0;
+        
+  
+    }
+
+    private static class Altura {
+     
+        public Altura() {
+        }
+    }
+     private static class base  {
+
+        public base() {
+        }
+    }
+   
+}
